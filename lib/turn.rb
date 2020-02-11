5abaa3702f66c3e3dd071899d7f4e84525883c62
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2020-02-11 06:09:46 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,8 @@
+def display_board(board)
+  separator = "-----------"
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts separator
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts separator
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
